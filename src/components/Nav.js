9a515d4ad4d5eceabef4b3b874636b8ce149aab1
luvi-kh <nalsengom@daum.net>
2024-09-22 17:54:11 +0900

    localStorage에 userData 담기

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,14 +5,16 @@ import {getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOu
 
 const Nav = () => {
 
+  const initialUserData = localStorage.getItem('userData') ?
+      JSON.parse(localStorage.getItem('userData')) : {};
+
   const [show, setShow] = useState(false);
   const {pathname} = useLocation();
   const [searchValue, setSearchValue] = useState("");
   const navigate = useNavigate();
   const auth = getAuth();
   const provider = new GoogleAuthProvider();
-
-  const [userData, setUserData] = useState({})
+  const [userData, setUserData] = useState(initialUserData)
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -24,7 +26,7 @@ const Nav = () => {
         navigate("/")
       }
     })
-  }, [])
+  }, [auth, navigate, pathname])
   
 
   useEffect(() => {
@@ -52,6 +54,7 @@ const Nav = () => {
     signInWithPopup(auth, provider)
     .then(result => {
       setUserData(result.user);
+      localStorage.setItem('userData', JSON.stringify(result.user));
     })
     .catch(error => {
       console.log(error);
