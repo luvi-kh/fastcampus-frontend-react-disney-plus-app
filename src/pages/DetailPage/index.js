199ef3f57d8158936633c1974c2686d8dceec97b
luvi-kh <nalsengom@daum.net>
2024-09-22 13:44:47 +0900

    useParams를 이용 detail page 생성

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import axios from '../../api/axios';
 
 const DetailPage = () => {
+  let {movieId} = useParams();
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(
+        `/movie/${movieId}`
+      )
+      setMovie(response.data);
+    }
+    fetchData();
+  }, [movieId])
+  
+  if(!movie) return null;
+
   return (
-    <div>DetailPage</div>
+    <section>
+      <img 
+        className='modal__poster-img'
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt="modal__poster-img"
+      />
+    </section>
   )
 }
 
