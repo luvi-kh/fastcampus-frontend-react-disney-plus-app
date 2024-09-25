28fb084d9c3f811dede60229446447aaa78499ab
luvi-kh <nalsengom@daum.net>
2024-09-26 04:38:21 +0900

    Redux-Persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+
+const rootReducer = combineReducers({
+  user: userReducer
+});
+
+const persistconfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistconfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer
-  }
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }    
+  })
 })
+
+export const persistor = persistStore(store);
